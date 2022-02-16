4d5de4fde10e7e6420fe5a335f68b6264a92b48a
Анастасия Крысько <99769003+yoursadcloud@users.noreply.github.com>
2022-02-16 11:22:59 +0400

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,49 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args){
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 22;
+        int b = 5;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 2002;
+        if (value <=0){
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers(){
+        int a = 34;
+        int b = 12;
+        if (a >= b) {
+            System.out.println("a>=b");
+        }
+        if (a < b) {
+            System.out.println("a<b");
+        }
+    }
+}
